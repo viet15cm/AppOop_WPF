4e15ef2d2f89b28b44c6c2f0a49d4b494fe79021
Administrator <Administrator@SHARE10S-PC>
2019-10-02 12:25:17 +0700

    Add project files.

---
+++ AppOOP/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AppOOP;
+namespace AppOOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTalk_Click(object sender, RoutedEventArgs e)
+        {
+            StudentCollege studentCollege = new StudentCollege();
+            studentCollege.StudentName = "Minh";
+            studentCollege.Talk();
+            StudentUniversity studentUniversity = new StudentUniversity();
+            studentUniversity.StudentName = "Dung";
+            studentUniversity.Talk();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ AppOOP/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace AppOOP
+{
+    class Student
+    {
+        public Student(string name)
+        {
+            this.name = name;
+        }
+
+        public Student()
+        {
+            
+        }
+
+        private string name;
+
+        public string StudentName { get => name; set => name = value; }
+
+
+        public virtual  void Talk()
+        {
+            if (StudentName.Equals("Minh"))
+                MessageBox.Show("My name is " + this.StudentName + " Iam a College Student !");
+            else if(StudentName.Equals("Dung"))
+                MessageBox.Show("My name is " + this.StudentName + " Iam a Universtiy Student !");
+            else
+                MessageBox.Show("errol");
+        }
+
+
+    }
+}
